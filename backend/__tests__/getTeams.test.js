d30b77d075c0014fa70c4e013b45466ba5858f47
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-08 01:40:42 +0100

    updated getTeams tests

--- backend/__tests__/getTeams.test.js
+++ backend/__tests__/getTeams.test.js
@@ -1,5 +1,27 @@
 import { getTeams } from "../src/getTeams";
 
-test('should return a permutation of teams in the list', () => { 
-    expect(getTeams(['Arsenal','Chelsea'])).toBe(console.log('Arsenal Chelsea\n Chelsea Arsenal'))
- })
+
+const teamList1 = ['Arsenal', 'Chelsea'];
+const teamList1Result = [['Arsenal','Chelsea'],["Chelsea",'Arsenal']]
+const wrongTeamList1Result = [['Arsenal','Chelsea'],["Chlsea",'rsenal']]
+const teamList2 = ['Arsenal', 'Chelsea','Liverpool'];
+const teamList2Result = [['Arsenal','Chelsea'],['Arsenal','Liverpool'],
+["Chelsea",'Arsenal'],['Chelsea','Liverpool'],
+['Liverpool','Arsenal'],['Liverpool',"Chelsea"]]
+const wrongTeamList2Result = [['Arsenal','Liverpool'],['Arsenal','Liverpool'],
+["Chelsea",'Arsenal'],['Chelsea','Liverpool'],
+['Liverpool','Arsenal'],['Liverpool',"Chelsea"]]
+
+describe('runs getTeams', () => {
+    test('should return a permutation of 2 teams', () => { 
+        expect(getTeams(teamList1)).toBe(teamList1Result)
+        expect(getTeams(teamList1)).not.toBe(wrongTeamList1Result)
+        
+    })
+
+    test('should return a permutation of 3 teams', () => {
+        expect(getTeams(teamList2)).toBe(teamList2Result)
+        expect(getTeams(teamList2)).not.toBe(wrongTeamList2Result)
+    })
+ 
+})
