d216a4982946eeb6ebb68a3459a589063ceedadc
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-08 16:46:41 +0100

    passes tests

--- backend/src/getTeams.js
+++ backend/src/getTeams.js
@@ -1,9 +1,15 @@
 export const getTeams = (teamList) => {
-    teamList.forEach(team => {
-        const filtered = teamList.filter(fTeamList => team != fTeamList);
-        filtered.forEach(team2 => {
-            console.log(team, team2);
-        });
+    const teamPairs = []
+    if(teamList.length > 1){
+        teamList.forEach(team => {
+            const filtered = teamList.filter(fTeamList => team != fTeamList);
+            filtered.forEach(team2 => {
+                
+                teamPairs.push([team,team2])
+                console.log(teamPairs)
+            });
 
-    });
+        });
+    }
+    return teamPairs
 };
