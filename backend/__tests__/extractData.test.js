6dd6b73efb9ae00dec1199df72fa7d044c047ef0
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-11 15:44:08 +0100

    extractData tests updated

--- backend/__tests__/extractData.test.js
+++ backend/__tests__/extractData.test.js
@@ -1,5 +1,19 @@
+import axios from "axios"
+import { extractDataResult } from "./teamListData"
 
 
 describe('runs extractData', () => { 
-    test('should return a list of objects containing the scores of both', () => { second })
+    
+    let teamData = axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=Arsenal_vs_Chelsea')
+    teamData = teamData.data
+    test('should return a list of objects containing the scores of both', () => { 
+        expect(teamData).toStrictEqual(extractDataResult)
+     })
+
+    test('should return a null list', () => { 
+        expect(undefined).toStrictEqual(extractDataResult)
+        expect({}).toStrictEqual(extractDataResult)
+     })
+
+   
  })
