f5eac56e7f84fbe88f39c8d3697064a7a260734f
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-12 19:06:35 +0100

    initial frontend commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className='btn btn-circle'> click</button>
+    </>
+  )
+}
+
+export default App
