f014544b1e5e38b7079f6929b8450733a1bab8eb
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-12 16:24:32 +0100

    updated test cases

--- backend/__tests__/getTeamPairs.test.js
+++ backend/__tests__/getTeamPairs.test.js
@@ -4,17 +4,17 @@ import { teamList1, teamList1Result, wrongTeamList1Result, teamList2, teamList2R
 
 describe('runs getTeamPairs', () => {
  
-    test('should return a permutation of 2 teams', () => { 
+    it('should return a permutation of 2 teams', () => { 
         expect(getTeamPairs(teamList1)).toStrictEqual(teamList1Result)
         expect(getTeamPairs(teamList1)).not.toStrictEqual(wrongTeamList1Result)
     })
 
-    test('should return a permutation of 3 teams', () => {
+    it('should return a permutation of 3 teams', () => {
         expect(getTeamPairs(teamList2)).toStrictEqual(teamList2Result)
         expect(getTeamPairs(teamList2)).not.toStrictEqual(wrongTeamList2Result)
     })
 
-    test('should return an empty list', () => {
+    it('should return an empty list', () => {
         expect(getTeamPairs([])).toStrictEqual([])
         expect(getTeamPairs(['Arsenal'])).toStrictEqual([])
     })
