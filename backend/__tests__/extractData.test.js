e27cd9caf8c639c6a9e59626f94f5f2be0406dfe
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-12 15:34:40 +0100

    added new test cases

--- backend/__tests__/extractData.test.js
+++ backend/__tests__/extractData.test.js
@@ -1,19 +1,33 @@
 import axios from "axios"
-import { extractDataResult } from "./teamListData"
+import { extractDataResult } from "../src/teamListData"
+import { extractData } from "../src/extractData"
 
 
 describe('runs extractData', () => { 
     
-    let teamData = axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=Arsenal_vs_Chelsea')
-    teamData = teamData.data
-    test('should return a list of objects containing the scores of both', () => { 
-        expect(teamData).toStrictEqual(extractDataResult)
+    
+    it('should return a list of objects containing the scores of both', async () => { 
+        let teamData = await axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=Arsenal_vs_Chelsea')
+        const result = extractData(teamData.data)
+        expect(result).toStrictEqual(extractDataResult)
+    })
+
+    it('should return an empty list if given a empty or undefined object', () => { 
+        expect(extractData(undefined)).toStrictEqual([])
+        expect(extractData({})).toStrictEqual([])
      })
 
-    test('should return a null list', () => { 
-        expect(undefined).toStrictEqual(extractDataResult)
-        expect({}).toStrictEqual(extractDataResult)
-     })
+    it('should return an empty list if the first team is invalid', async () => { 
+        
+        let teamData = await axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=fyuebj_vs_Chelsea')
+        const result = extractData(teamData.data)
+        expect(result).toStrictEqual([])
+    })
 
+    it('should return an empty list if the second team is invalid', async () => { 
+        let teamData = await axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=Arsenal_vs_Hdjek')
+        const result = extractData(teamData.data)
+        expect(result).toStrictEqual([])
+    })
    
  })
