b04cb6f3c7207d8cac95d11ae0fc7b02b2bb0549
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-13 20:02:58 +0100

    fixed server

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,18 +7,26 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req,res) => { 
+app.get('/', async (req,res) => { 
     
     try {
-        let matches = []
         const teamList = req.query.teams.split(',')
+        const matchData = {}
+        teamList.forEach(team => {
+            matchData[team]= []
+        })
         const teamPairs = getTeamPairs(teamList)
-        teamPairs.forEach(async (pair) => {
+
+        // forEach doesn't return return promises with async so use map instead 
+        const  matchDataPromises = teamPairs.map(async (pair) => {
             const teamData = await axios.get(
                 `https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=${pair[0]}_vs_${pair[1]}`)
-            matches = extractData(teamData.data)
-            res.sendStatus(200).json(matches)
+            
+            const extractedData = extractData(teamData.data)
+            matchData[pair[0]].push(extractedData)
         })
+        await Promise.all(matchDataPromises)
+        res.status(200).json(matchData)
     } catch (error) {
         console.log('Error getting data\n(ERROR):',error)
         res.sendStatus()
