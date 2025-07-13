1e562a4686482789269b0aeace74aee3fc9c3583
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-13 18:11:39 +0100

    fixed function

--- backend/src/extractData.js
+++ backend/src/extractData.js
@@ -5,7 +5,6 @@ export const extractData = (teamData) => {
         teamData.event.filter(e => e.strLeague == "English Premier League")
         .forEach(matchData => {
             const resultData = {
-                homeTeam: matchData.strHomeTeam,
                 awayTeam: matchData.strAwayTeam,
                 homeScore: parseInt(matchData.intHomeScore),
                 awayScore: parseInt(matchData.intAwayScore)
