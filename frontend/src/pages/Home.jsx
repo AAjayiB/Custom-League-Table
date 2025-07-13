048ac70b344ed7e91378d5c876cd89694e1beb0e
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-13 15:31:26 +0100

    pages created

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LeagueTable from '../components/LeagueTable'
+
+const Home = () => {
+  return (
+    <>
+      <LeagueTable />
+    </>
+  )
+}
+
+export default Home
