620058ac7aa1085420df3acbbe3bacc2fdb1ed76
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-13 17:29:53 +0100

    league table added for viewing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import LeagueTable from '../components/LeagueTable'
 
+const teams = ['Arsenal','Chelsea','Liverpool']
 const Home = () => {
   return (
     <>
-      <LeagueTable />
+      <LeagueTable teams={teams} />
     </>
   )
 }
