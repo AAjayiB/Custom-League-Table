05d9c83f59865b1c017e80703ff5d36781017738
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-13 15:31:13 +0100

    routing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import LeagueTable from './components/LeagueTable'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Result from './pages/Result'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <button className='btn btn-circle'> click</button>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/results' element={<Result />} />
+    </Routes>
   )
 }
 
