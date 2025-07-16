858f4c75aef6c444c0429c3a93636e12540d528e
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-16 21:32:50 +0100

    updated test cases

--- backend/__tests__/extractData.test.js
+++ backend/__tests__/extractData.test.js
@@ -9,22 +9,22 @@ describe('runs extractData', () => {
     it('should return a list of objects containing the scores of both', async () => { 
         let teamData = await axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=Arsenal_vs_Chelsea')
         const result = extractData(teamData.data)
-        expect(result).toStrictEqual(expect.arrayContaining([extractDataResult]))
+        expect(result).toStrictEqual(extractDataResult)
     })
 
-    it('should return an empty list if given a empty or undefined object', () => { 
+    it('should return undefined if given a empty or undefined object', () => { 
         expect(extractData(undefined)).toStrictEqual([])
-        expect(extractData({})).toStrictEqual([])
+        expect(extractData({})).toStrictEqual(undefined)
      })
 
-    it('should return an empty list if the first team is invalid', async () => { 
+    it('should return undefined if the first team is invalid', async () => { 
         
         let teamData = await axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=fyuebj_vs_Chelsea')
         const result = extractData(teamData.data)
         expect(result).toStrictEqual([])
     })
 
-    it('should return an empty list if the second team is invalid', async () => { 
+    it('should return undefined if the second team is invalid', async () => { 
         let teamData = await axios.get('https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=Arsenal_vs_Hdjek')
         const result = extractData(teamData.data)
         expect(result).toStrictEqual([])
