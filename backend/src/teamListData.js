81cc0b0c85e0a3da1e445318b0d0404a6d400efc
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-16 21:32:23 +0100

    added new test data

--- backend/src/teamListData.js
+++ backend/src/teamListData.js
@@ -14,3 +14,28 @@ export const extractDataResult = {
     homeScore: 1,
     awayScore: 0
 }
+
+export const processDataResult = [
+    {
+    team:'Arsenal',
+    matchesPlayed: 2,
+    wins: 1,
+    draws: 1,
+    losses: 0,
+    points: 4,
+    goalFor: 7,
+    goalAgainst: 2,
+    goalDiff: 5
+    },
+    {
+    team:'Chelsea',
+    matchesPlayed: 2,
+    wins: 0,
+    draws: 1,
+    losses: 1,
+    points: 1,
+    goalFor: 2,
+    goalAgainst: 7,
+    goalDiff: -5
+    }
+]
