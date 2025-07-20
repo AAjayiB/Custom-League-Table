cd51b51562e6fb2195fa12c4fc2e9c99d2ec9c57
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-20 18:02:28 +0100

    returned data using useQuery

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import LeagueTable from '../components/LeagueTable'
 import Header from '../components/Header'
-
+import {useQuery} from '@tanstack/react-query'
+import { getMatchData } from '../api/api'
 const Result = ({teams}) => {
 
+  // const getMatchData2 = getMatchData(teams)
+  
+  const {data, status, error, isLoading} = useQuery({
+    queryKey: ['matches'],
+    queryFn: async () => {
+      const dd = await getMatchData(teams)
+      return dd
+    }
+
+  })
+  console.log(data)
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (status === 'error') {
+    return <span>Error: {error.message}</span>
+  }
   
   return (
     <>
-    <Header />
-      <LeagueTable teams={teams} />
+    {/* <Header />
+      <LeagueTable teams={teams} /> */}
+      <div>{JSON.stringify(data)}</div>
     </>
   )
 }
