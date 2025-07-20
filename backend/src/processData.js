46e4ca8a6aecc362744facdabb53545593961f2d
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-21 00:25:43 +0100

    added comments to code

--- backend/src/processData.js
+++ backend/src/processData.js
@@ -1,71 +1,76 @@
 export const processData = (extractedData) => {
+    // Checks if data exists
     if(extractedData){
-        console.log(extractedData)
+
+        // Creates a list from the keys in the data
         const teamKeys = Object.keys(extractedData)
+        
+        // User-defined object the results will be stored in
         function resultModel(team){this.team = team, this.matchesPlayed=0, this.wins=0, this.draws=0, 
             this.losses=0, this.points=0, this.goalFor=0, this.goalAgainst=0, this.goalDiff=0
         }
         const result =[]
+
         teamKeys.forEach((key) =>{
+            // computes all the data for a team's home games
             let allGames = extractedData[key].reduce((resultObject,gameData) => {
-                // console.log(resultObject)
                resultObject =  {...resultObject, 
                 matchesPlayed: resultObject.matchesPlayed + 1, 
                 goalFor: resultObject.goalFor + gameData.homeScore,
                 goalAgainst: resultObject.goalAgainst + gameData.awayScore
-            }
-            // console.log(resultObject)
+            } 
             if(gameData.homeScore > gameData.awayScore) resultObject = {...resultObject, wins: resultObject.wins + 1}
             else if(gameData.homeScore === gameData.awayScore) resultObject = {...resultObject, draws: resultObject.draws + 1}
             else resultObject = {...resultObject, losses: resultObject.losses + 1 }
-            // console.log(resultObject)
             return resultObject
-            
             },  new resultModel(key.replace('_'," "),0,0,0,0,0,0,0,0))
-            // console.log(homeGames)
-            //
-            // FINISH THE REST OF THE UPDATE CODE FOR AWAY GAMES THEN CALCULATE THE DERIVED VALUES (POINTS AND GOALDIFF)
-            
+     
+            // add the data for a team's away games to allGames to get data for both home and away games           
             const teamKeys2 = teamKeys.filter((teamKeys2) => key != teamKeys2)
             teamKeys2.forEach(key2 => extractedData[key2]
             .filter(awayGames => awayGames.awayTeam == key.replace('_'," "))
             .reduce((resultObject,gameData) =>{
-                // console.log(resultObject)
                 resultObject =  {...resultObject, 
                     matchesPlayed: resultObject.matchesPlayed + 1, 
                     goalFor: resultObject.goalFor + gameData.awayScore,
                     goalAgainst: resultObject.goalAgainst + gameData.homeScore
                 }
-                // console.log(resultObject)
             if(gameData.homeScore < gameData.awayScore) resultObject = {...resultObject, wins: resultObject.wins + 1}
             else if(gameData.homeScore === gameData.awayScore) resultObject = {...resultObject, draws: resultObject.draws + 1}
             else resultObject = {...resultObject, losses: resultObject.losses + 1 }
-            // console.log(resultObject)
             allGames = resultObject
             },allGames))
+
+            // computes derived values (points & goal difference) for all a team's games
             allGames = {...allGames, points: (3*allGames.wins)+allGames.draws, goalDiff: allGames.goalFor-allGames.goalAgainst}
-            // console.log(allGames)
+            
+            // push the team's league statistics into the result array
             result.push(allGames)
         })
-        // console.log(result)
+
+        // ranks the teams within the array  
         return result.sort(compareGoalFor).sort(compareGoalDiff).sort(comparePoints).reverse()
     }
+
+    // returns an empty list if extractedData is empty
     else return [];
 }  
 
-
+// compares goal difference between 2 teams
 function compareGoalDiff(team1,team2) {
     if(team1.goalDiff < team2.goalDiff) return -1
     else if(team1.goalDiff > team2.goalDiff) return 1
     else return 0
 }
 
+// compares goals for between 2 teams
 function compareGoalFor(team1,team2) {
     if(team1.goalFor < team2.goalFor) return -1
     else if(team1.goalFor > team2.goalFor) return 1
     else return 0
 }
 
+// compares points between 2 teams
 function comparePoints(team1,team2) {
     if(team1.points < team2.points) return -1
     else if(team1.points > team2.points) return 1
