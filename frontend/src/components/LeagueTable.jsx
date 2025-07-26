a1040a86559e6fbc5794a5a07aa09e54f304d38b
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-26 21:51:34 +0100

    added stylings to result page

--- frontend/src/components/LeagueTable.jsx
+++ frontend/src/components/LeagueTable.jsx
@@ -1,53 +1,34 @@
-import React from 'react'
-import LeagueTableSlot from './LeagueTableSlot'
+import React from "react";
+import LeagueTableSlot from "./LeagueTableSlot";
+import "../styles/LeagueTable.css";
 
-const LeagueTable = ({teams}) => {
+const LeagueTable = ({ teams }) => {
   return (
-    <div className='overflow-x-auto rounded-box py-10'>
-        <table className='table table-zebra'>
-            <thead className='bg-black'>
-                <tr>
-                    <th>
-                        #
-                    </th>
-                    <th>
-                        Team
-                    </th>
-                    <th>
-                        MP
-                    </th>
-                    <th>
-                        W
-                    </th>
-                    <th>
-                        D
-                    </th>
-                    <th>
-                        L
-                    </th>
-                    <th>
-                        Pts
-                    </th>
-                    <th>
-                        GF
-                    </th>
-                    <th>
-                        GA
-                    </th>
-                    <th>
-                        GD
-                    </th>
-                </tr>
-            </thead>
-           
-            <tbody>
-                {teams.map((team,index) => (
-                   <LeagueTableSlot index={index} team={team}/>
-                ))}
-            </tbody>
-        </table>
-    </div>
-  )
-}
+    <div className="overflow-x-auto rounded-box py-10 m-10">
+      <table className="table table-zebra">
+        <thead className="bg-black">
+          <tr>
+            <th>#</th>
+            <th>Team</th>
+            <th>MP</th>
+            <th>W</th>
+            <th>D</th>
+            <th>L</th>
+            <th>Pts</th>
+            <th>GF</th>
+            <th>GA</th>
+            <th>GD</th>
+          </tr>
+        </thead>
 
-export default LeagueTable
+        <tbody className="even:bg-blue-500 odd:bg-amber-">
+          {teams.map((team, index) => (
+            <LeagueTableSlot index={index} team={team} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default LeagueTable;
--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import LeagueTable from '../components/LeagueTable'
-import Header from '../components/Header'
-import {useQuery} from '@tanstack/react-query'
-import { getMatchData } from '../api/api'
-const Result = ({teams}) => {
-
+import React from "react";
+import LeagueTable from "../components/LeagueTable";
+import Header from "../components/Header";
+import { useQuery } from "@tanstack/react-query";
+import { getMatchData } from "../api/api";
+const Result = ({ teams }) => {
   // const getMatchData2 = getMatchData(teams)
-  
-  const {data, status, error, isLoading} = useQuery({
-    queryKey: ['matches'],
+
+  const { data, status, error, isLoading } = useQuery({
+    queryKey: ["matches"],
     queryFn: async () => {
-      const dd = await getMatchData(teams)
-      return dd
-    }
-
-  })
-  console.log(data)
+      const dd = await getMatchData(teams);
+      return dd;
+    },
+  });
+  console.log(data);
   if (isLoading) {
-    return <span>Loading...</span>
+    return (
+      <>
+        <Header />
+        <div className="skeleton h-50 m-10  bg-violet-50"></div>
+      </>
+    );
   }
 
-  if (status === 'error') {
-    return <span>Error: {error.message}</span>
+  if (status === "error") {
+    return (
+      <>
+        <Header />
+        <div className="text-black text-3xl text-center">
+          Error: {error.message}
+        </div>
+      </>
+    );
   }
-  
+
   return (
     <>
-    {/* <Header /> */}
+      <Header />
       <LeagueTable teams={data} />
-      {/* <div>{JSON.stringify(data)}</div> */}
     </>
-  )
-}
+  );
+};
 
-export default Result
+export default Result;
