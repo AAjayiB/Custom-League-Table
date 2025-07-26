704cd991b1d510b17f1cc04c92f748a831579c30
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-26 21:50:53 +0100

    added more configurations to queryclient

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router'
-import './index.css'
-import App from './App.jsx'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false
-    }
-  }
-})
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-       <App />
-    </QueryClientProvider>
-     
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
