850dd63b62448ee57c332a864540bbe13dd3ce4a
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-07-28 00:03:10 +0100

    updated for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,68 +1,97 @@
-import express from 'express'
-import axios from 'axios'
-import cors from 'cors'
-import { getTeamPairs } from './getTeamPairs.js'
-import { extractData } from './extractData.js'
-import { processData } from './processData.js'
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import { getTeamPairs } from "./getTeamPairs.js";
+import { extractData } from "./extractData.js";
+import { processData } from "./processData.js";
 
-const app = express()
+dotenv.config({ path: "./.env" });
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: 'GET'
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+  const corsOptions = {
+    origin: "http://localhost:5173",
+    methods: "GET",
+  };
+  app.use(cors(corsOptions));
 }
-app.use(cors(corsOptions))
-app.use(express.json())
 
-app.get('/', async (req,res) => { 
-    
-    try {
-        // const teamList = req.query.teams.split(',')
-        // max is 6 teams because of rate limiting constraints
-        // to increase max teams add extra query parameter
-        const queryTeams = [req.query.t1,req.query.t2,req.query.t3,req.query.t4,req.query.t5,req.query.t6]
+app.get("/api", async (req, res) => {
+  try {
+    // const teamList = req.query.teams.split(',')
+    // max is 6 teams because of rate limiting constraints
+    // to increase max teams add extra query parameter
+    const queryTeams = [
+      req.query.t1,
+      req.query.t2,
+      req.query.t3,
+      req.query.t4,
+      req.query.t5,
+      req.query.t6,
+    ];
 
-        // remove undefined team parameters and duplicates
-        const teamList= queryTeams.filter((team , index, array) => team && array.indexOf(team) === index)
+    // remove undefined team parameters and duplicates
+    const teamList = queryTeams.filter(
+      (team, index, array) => team && array.indexOf(team) === index
+    );
 
-        //create the object returned and use the valid team parameters as keys
-        const matchData = {}
-        teamList.forEach(team => {
-            matchData[team]= []
-        })
+    //create the object returned and use the valid team parameters as keys
+    const matchData = {};
+    teamList.forEach((team) => {
+      matchData[team] = [];
+    });
 
-        // return permutations of the teams
-        const teamPairs = getTeamPairs(teamList)
+    // return permutations of the teams
+    const teamPairs = getTeamPairs(teamList);
 
-        // forEach doesn't return return promises with async so use map instead 
-        // list of promises returned 
-        const  matchDataPromises = teamPairs.map(async (pair) => {
-            const teamData = await axios.get(
-                `https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=${pair[0]}_vs_${pair[1]}`)
-            
-            const extractedData = extractData(teamData.data,pair[1])
-            matchData[pair[0]].push(extractedData)
-        })
+    // forEach doesn't return return promises with async so use map instead
+    // list of promises returned
+    const matchDataPromises = teamPairs.map(async (pair) => {
+      const teamData = await axios.get(
+        `https://www.thesportsdb.com/api/v1/json/123/searchevents.php?s=2024-2025&e=${pair[0]}_vs_${pair[1]}`
+      );
 
-        // accept the list of promises
-        await Promise.all(matchDataPromises)
-       
+      const extractedData = extractData(teamData.data, pair[1]);
+      matchData[pair[0]].push(extractedData);
+    });
 
-        //// Process here!!! 
-        const results = processData(matchData)
-        // send the match data
-        return res.status(200).json(results)
-    } catch (error) {
-        console.log(error.message)
-        if(error.status === 429) return res.status(429).json({status:'Failed',code:error.status,message:'Too many requests right now. Please try again after 1 minute'})
-        else return res.status(500).json({status:'Failed', code:error.status, message:error.message})
-        
-    }
-    
-})
+    // accept the list of promises
+    await Promise.all(matchDataPromises);
 
-app.listen(5000, () => {
-    console.log('Listening at PORT 5000')
-})
+    //// Process here!!!
+    const results = processData(matchData);
+    // send the match data
+    return res.status(200).json(results);
+  } catch (error) {
+    console.log(error.message);
+    if (error.status === 429)
+      return res.status(429).json({
+        status: "Failed",
+        code: error.status,
+        message: "Too many requests right now. Please try again after 1 minute",
+      });
+    else
+      return res
+        .status(500)
+        .json({ status: "Failed", code: error.status, message: error.message });
+  }
+});
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Listening at PORT", PORT);
+});
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,18 +1,28 @@
-import axios from 'axios'
+import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
-export const getMatchData = async(params) => {
-    const data = await axios.get('http://localhost:5000',{
-        params: {
-            t1:params.t1,
-            t2:params.t2,
-            t3:params.t3,
-            t4:params.t4,
-            t5:params.t5,
-            t6:params.t6
-        }
-    })
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const getMatchData = async (params) => {
+  try {
+    const data = await api.get("/", {
+      params: {
+        t1: params.t1,
+        t2: params.t2,
+        t3: params.t3,
+        t4: params.t4,
+        t5: params.t5,
+        t6: params.t6,
+      },
+    });
     // if data isn't showning up for no particular reasoning add conditional chaining (?.)
-    return data.data
-    
-}
+    return data.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
